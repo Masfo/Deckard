db0481aa9005d9b34a0a1a3fd46eebd7a0c93284
Toni Mäkinen <masfonos@gmail.com>
2024-03-31 17:10:58 +0300

    SCD: Testing enum flags

--- script/main.cpp
+++ script/main.cpp
@@ -1,10 +1,54 @@
+
+#include <signal.h>
+#include <stdlib.h>
+
 import std;
 import Deckard;
 
 using namespace deckard;
 
+namespace Filesystem
+{
+	enum class Permission : u8
+	{
+		NoPermissions = 0x00,
+		Read          = 0x01,
+		Write         = 0x02,
+		Execute       = 0x04,
+	};
+
+
+	consteval void enable_bitmask_operations(Permission);
+
+
+	/*
+		using Filesystem::Permission;
+		Permission readAndWrite{Permission::Read | Permission::Write};
+	*/
+} // namespace Filesystem
+
 int main()
 {
+
+
+	using Filesystem::Permission;
+
+
+	Permission readAndWrite{Permission::Read | Permission::Write | Permission::Execute};
+
+
+	readAndWrite -= Permission::Write;
+
+	readAndWrite -= Permission::Read;
+
+	readAndWrite -= Permission::Execute;
+
+	readAndWrite += Permission::Execute;
+	readAndWrite += Permission::Write;
+
+
+	//	std::println("{}", readAndWrite);
+
 	std::println("Script Compiler v5");
 
 	return 0;
