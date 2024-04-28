ebdf7ebadf2b653501f32eaf6a8a87b0c5e77144
Toni Mäkinen <masfonos@gmail.com>
2024-04-28 21:08:16 +0300

    base64: add decode tests

--- tests/tests/base64_test.cpp
+++ tests/tests/base64_test.cpp
@@ -7,23 +7,44 @@ import std;
 using namespace deckard::utils;
 using namespace std::string_literals;
 
-TEST_SUITE("base64" * doctest::description("base64"))
+TEST_SUITE("encode base64" * doctest::description("encode base64"))
 {
-	TEST_CASE("base64('foob') - w/ padding")
+	TEST_CASE("encode base64('foob') - w/ padding")
 	{
 		//
 		CHECK_EQ(base64::encode_str("foob"), "Zm9vYg=="s);
 	}
 
-	TEST_CASE("base64('foob') - w/o padding")
+	TEST_CASE("encode base64('foob') - w/o padding")
 	{
 		//
 		CHECK_EQ(base64::encode_str("foob", base64::padding::no), "Zm9vYg"s);
 	}
 
-	TEST_CASE("base64('sphinx....')")
+	TEST_CASE("encode base64('sphinx....')")
 	{
 		//
 		CHECK_EQ(base64::encode_str("Sphinx of black quartz, judge my vow"), "U3BoaW54IG9mIGJsYWNrIHF1YXJ0eiwganVkZ2UgbXkgdm93"s);
+	}
+}
+
+TEST_SUITE("decode base64" * doctest::description("decode base64"))
+{
+	TEST_CASE("decode base64('foob') - w/ padding")
+	{
+		//
+		CHECK_EQ(base64::decode_str("Zm9vYg=="), "foob"s);
+	}
+
+	TEST_CASE("decode base64('foob') - w/o padding")
+	{
+		//
+		CHECK_EQ(base64::decode_str("Zm9vYg"), "foob"s);
+	}
+
+	TEST_CASE("decode base64('sphinx....')")
+	{
+		//
+		CHECK_EQ(base64::decode_str("U3BoaW54IG9mIGJsYWNrIHF1YXJ0eiwganVkZ2UgbXkgdm93"), "Sphinx of black quartz, judge my vow"s);
 	}
 }
