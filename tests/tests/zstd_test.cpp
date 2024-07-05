64d5da8d627c75a8768aff33e9c8a0196967b107
Toni Mäkinen <masfonos@gmail.com>
2024-07-05 18:40:25 +0300

    deckard: zstd test

---
+++ tests/tests/zstd_test.cpp
@@ -0,0 +1,31 @@
+#include <catch2/catch_test_macros.hpp>
+
+import deckard.types;
+import deckard.zstd;
+import std;
+
+using namespace deckard;
+
+TEST_CASE("zstd", "[zstd]")
+{
+	std::vector<u8> buffer(256);
+	std::vector<u8> comp;
+	comp.resize(zstd::bound(buffer));
+
+	// Compress
+	auto result = zstd::compress(buffer, comp);
+	REQUIRE(result != std::nullopt);
+	comp.resize(*result);
+
+	// Uncompress
+	std::vector<u8> uncompressed;
+	if (auto s = zstd::uncompressed_size(comp); s)
+		uncompressed.resize(*s);
+	else
+		REQUIRE(s != std::nullopt);
+
+
+	result = zstd::uncompress(comp, uncompressed);
+
+	REQUIRE(result != std::nullopt);
+}
