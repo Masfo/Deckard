535a2fce35b328af926f19266e8b5fa898fcef25
Toni Mäkinen <masfonos@gmail.com>
2024-08-24 17:52:55 +0300

    function_ref: tests

--- tests/tests/function_ref_test.cpp
+++ tests/tests/function_ref_test.cpp
@@ -6,17 +6,21 @@ import std;
 
 using namespace deckard;
 
-bool function_call = false;
-bool method_call   = false;
+int function_call = 0;
+int method_call   = 0;
 
-struct b
+struct MyClass
 {
-	int voidme(int x) { return x + 42; }
+	int method(int x)
+	{
+		method_call += 1;
+		return x + 42;
+	}
 };
 
 bool f(bool b)
 {
-	function_call = true;
+	function_call += 1;
 	return not b;
 }
 
@@ -27,7 +31,7 @@ TEST_CASE("function_ref", "[function_ref][utils]")
 	{
 		function_ref<bool(bool)> fr = f;
 		REQUIRE(true == fr(false));
-		REQUIRE(true == function_call);
+		REQUIRE(1 == function_call);
 	}
 
 	// lambda
@@ -36,10 +40,17 @@ TEST_CASE("function_ref", "[function_ref][utils]")
 		REQUIRE(fr(8) == 50);
 	}
 
+	{
+		const int              value = 10;
+		function_ref<int(int)> fr    = [&value](int x) { return x + 42 + value; };
+		REQUIRE(fr(8) == 60);
+	}
+
 	// method
 	{
-		b                          o;
-		function_ref<int(b&, int)> fr = &b::voidme;
-		REQUIRE(fr(o, 8) == 50);
+		MyClass                          C;
+		function_ref<int(MyClass&, int)> fr = &MyClass::method;
+		REQUIRE(fr(C, 8) == 50);
+		REQUIRE(1 == method_call);
 	}
 }
