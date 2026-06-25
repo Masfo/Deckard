bccadd00ab095ffcdafc2fccc6315d2227174d8f
Toni Mäkinen <masfonos@gmail.com>
2026-06-25 07:57:41 +0300

    arena: stackarena

--- memory/arena.ixx
+++ memory/arena.ixx
@@ -9,6 +9,73 @@ namespace deckard::memory
 	template<typename T>
 	concept trivially_destructible = std::is_trivially_destructible_v<T>;
 
+	// ###########################################################################
+
+
+	export template<size_t N>
+	class stackarena
+	{
+	private:
+		std::array<std::byte, N> m_buffer{};
+		u64                      m_offset{0};
+
+		[[nodiscard]] void* raw_allocate(u64 size_in_bytes, u64 align = alignof(std::max_align_t))
+		{
+			void* base    = m_buffer.data() + m_offset;
+			u64   space   = N - m_offset;
+			void* aligned = std::align(align, size_in_bytes, base, space);
+			if (not aligned)
+				return nullptr;
+
+			m_offset = static_cast<std::byte*>(aligned) - m_buffer.data() + size_in_bytes;
+			return aligned;
+		}
+
+	public:
+		template<trivially_destructible T = std::byte>
+		[[nodiscard]] std::span<T> allocate(u64 count = 1)
+		{
+			void* mem = raw_allocate(sizeof(T) * count, alignof(T));
+			if (not mem)
+				return {};
+			return {static_cast<T*>(mem), count};
+		}
+
+		template<trivially_destructible T, typename... Args>
+		[[nodiscard]] T* create(Args&&... args)
+		{
+			void* mem = raw_allocate(sizeof(T), alignof(T));
+			if (not mem)
+				return nullptr;
+
+			return std::construct_at(static_cast<T*>(mem), std::forward<Args>(args)...);
+		}
+
+		template<trivially_destructible T, typename... Args>
+		[[nodiscard]] std::span<T> create_array(u64 count, Args&&... args)
+		{
+			void* mem = raw_allocate(sizeof(T) * count, alignof(T));
+			if (not mem)
+				return {};
+
+			std::span<T> result(static_cast<T*>(mem), count);
+			for (auto& elem : result)
+				std::construct_at(&elem, args...);
+
+			return result;
+		}
+
+		void reset() { m_offset = 0; }
+
+		u64 capacity() const { return N; }
+
+		u64 used() const { return m_offset; }
+
+		u64 free() const { return N - m_offset; }
+	};
+
+	// ###########################################################################
+
 	export class arena
 	{
 	private:
@@ -16,15 +83,7 @@ namespace deckard::memory
 		u64       m_offset{0};
 		u64       m_capacity{0};
 
-
-	public:
-		explicit arena(u64 capacity_in_bytes)
-			: m_buffer(make_bytearray(capacity_in_bytes))
-			, m_capacity(capacity_in_bytes)
-		{
-		}
-
-		[[nodiscard]] void* allocate(u64 size_in_bytes, u64 align = alignof(std::max_align_t))
+		[[nodiscard]] void* raw_allocate(u64 size_in_bytes, u64 align = alignof(std::max_align_t))
 		{
 			void* base  = m_buffer.get() + m_offset;
 			u64   space = m_capacity - m_offset;
@@ -37,21 +96,26 @@ namespace deckard::memory
 			return aligned;
 		}
 
-
-		template<trivially_destructible T>
-		[[nodiscard]] std::span<T> allocate_block(u64 count)
+	public:
+		explicit arena(u64 capacity_in_bytes)
+			: m_buffer(make_bytearray(capacity_in_bytes))
+			, m_capacity(capacity_in_bytes)
 		{
-			void* mem = allocate(sizeof(T) * count, alignof(T));
+		}
+
+		template<trivially_destructible T = std::byte>
+		[[nodiscard]] std::span<T> allocate(u64 count = 1)
+		{
+			void* mem = raw_allocate(sizeof(T) * count, alignof(T));
 			if (not mem)
 				return {};
-
-			return std::span<T>(static_cast<T*>(mem), count);
+			return {static_cast<T*>(mem), count};
 		}
 
 		template<trivially_destructible T, typename... Args>
 		[[nodiscard]] T* create(Args&&... args)
 		{
-			void* mem = allocate(sizeof(T), alignof(T));
+			void* mem = raw_allocate(sizeof(T), alignof(T));
 			if (not mem)
 				return nullptr;
 
@@ -61,7 +125,7 @@ namespace deckard::memory
 		template<trivially_destructible T, typename... Args>
 		[[nodiscard]] std::span<T> create_array(u64 count, Args&&... args)
 		{
-			void* mem = allocate(sizeof(T) * count, alignof(T));
+			void* mem = raw_allocate(sizeof(T) * count, alignof(T));
 			if (not mem)
 				return {};
 
--- tests/tests/memory_test.cpp
+++ tests/tests/memory_test.cpp
@@ -7,6 +7,90 @@ import std;
 import deckard.types;
 import deckard.memory;
 
+TEST_CASE("stack arena", "[arena][memory]")
+{
+	using namespace deckard;
+
+	struct Particle
+	{
+		f32 x, y, z;
+	};
+
+	SECTION("stackarena ctor")
+	{
+		memory::stackarena<1024> frame;
+		CHECK(frame.capacity() == 1024);
+		CHECK(frame.used() == 0);
+		CHECK(frame.free() == 1024);
+	}
+	SECTION("stackarena reset")
+	{
+		memory::stackarena<1024> frame;
+		auto                     ptr1 = frame.allocate(128);
+		CHECK(not ptr1.empty());
+		CHECK(frame.used() == 128);
+		CHECK(frame.free() == 896);
+		frame.reset();
+		CHECK(frame.used() == 0);
+		CHECK(frame.free() == 1024);
+	}
+
+	SECTION("stackarena allocate")
+	{
+		memory::stackarena<1024> frame;
+		auto                     ptr1 = frame.allocate(128);
+		CHECK(frame.used() == 128);
+		CHECK(frame.free() == 896);
+		auto ptr2 = frame.allocate(256);
+		CHECK(frame.used() == 384);
+		CHECK(frame.free() == 640);
+		
+		auto ptr3 = frame.allocate(700); // Should fail
+		CHECK(ptr3.size() == 0);
+		CHECK(frame.used() == 384);
+		CHECK(frame.free() == 640);
+
+		frame.reset();
+		CHECK(frame.used() == 0);
+		CHECK(frame.free() == 1024);
+	}
+
+	SECTION("stackarena create particles")
+	{
+		memory::stackarena<1024> frame;
+		CHECK(frame.capacity() == 1024);
+		auto* p = frame.create<Particle>(1.0f, 2.0f, 3.0f);
+		CHECK(p != nullptr);
+		CHECK(frame.used() == sizeof(Particle));
+		CHECK(frame.free() == 1024 - sizeof(Particle));
+		CHECK(p->x == 1.0f);
+		CHECK(p->y == 2.0f);
+		CHECK(p->z == 3.0f);
+	}
+
+	SECTION("stackarena create array of particles")
+	{
+		memory::stackarena<1024> frame;
+		CHECK(frame.capacity() == 1024);
+		auto particles = frame.create_array<Particle>(5);
+		CHECK(particles.size() == 5);
+		CHECK(frame.used() == sizeof(Particle) * 5);
+		CHECK(frame.free() == 1024 - sizeof(Particle) * 5);
+		for (size_t i = 0; i < particles.size(); ++i)
+		{
+			particles[i].x = static_cast<f32>(i);
+			particles[i].y = static_cast<f32>(i + 1);
+			particles[i].z = static_cast<f32>(i + 2);
+		}
+		for (size_t i = 0; i < particles.size(); ++i)
+		{
+			CHECK(particles[i].x == static_cast<f32>(i));
+			CHECK(particles[i].y == static_cast<f32>(i + 1));
+			CHECK(particles[i].z == static_cast<f32>(i + 2));
+		}
+	}
+}
+
 TEST_CASE("arena", "[arena][memory]")
 {
 	using namespace deckard;
@@ -23,7 +107,7 @@ TEST_CASE("arena", "[arena][memory]")
 	{
 		memory::arena frame(1024);
 		auto          ptr1 = frame.allocate(128);
-		CHECK(ptr1 != nullptr);
+		CHECK(not ptr1.empty());
 		CHECK(frame.used() == 128);
 		CHECK(frame.free() == 896);
 		frame.reset();
@@ -34,17 +118,14 @@ TEST_CASE("arena", "[arena][memory]")
 	SECTION("arena allocate")
 	{
 		memory::arena frame(1024);
-		void*         ptr1 = frame.allocate(128);
-		CHECK(ptr1 != nullptr);
+		auto          ptr1 = frame.allocate(128);
 		CHECK(frame.used() == 128);
 		CHECK(frame.free() == 896);
-		void* ptr2 = frame.allocate(256);
-		CHECK(ptr2 != nullptr);
+		auto ptr2 = frame.allocate(256);
 		CHECK(frame.used() == 384);
 		CHECK(frame.free() == 640);
 
-		void* ptr3 = frame.allocate(700); // Should fail
-		CHECK(ptr3 == nullptr);
+		auto ptr3 = frame.allocate(700); // Should fail
 		CHECK(frame.used() == 384);
 		CHECK(frame.free() == 640);
 
@@ -53,33 +134,12 @@ TEST_CASE("arena", "[arena][memory]")
 		CHECK(frame.free() == 1024);
 	}
 
-	SECTION("arena allocate with alignment")
-	{
-		memory::arena frame(1024);
-		void*         ptr1 = frame.allocate(128, 16);
-		CHECK(ptr1 != nullptr);
-		CHECK(frame.used() == 128);
-		CHECK(frame.free() == 896);
-		CHECK(reinterpret_cast<std::uintptr_t>(ptr1) % 16 == 0);
-		void* ptr2 = frame.allocate(256, 32);
-		CHECK(ptr2 != nullptr);
-
-		auto base          = reinterpret_cast<std::uintptr_t>(frame.data().data());
-		auto expected_used = reinterpret_cast<std::uintptr_t>(ptr2) - base + 256;
-		CHECK(frame.used() == expected_used);
-		CHECK(frame.free() == 1024 - expected_used);
-		CHECK(reinterpret_cast<std::uintptr_t>(ptr2) % 32 == 0);
-
-		frame.reset();
-		CHECK(frame.used() == 0);
-		CHECK(frame.free() == 1024);
-	}
 
 	SECTION("arena allocate oversized")
 	{
 		memory::arena frame(1024);
-		void*         ptr1 = frame.allocate(2048);
-		CHECK(ptr1 == nullptr);
+		auto          ptr1 = frame.allocate(2048);
+		CHECK(ptr1.size() == 0);
 		CHECK(frame.used() == 0);
 		CHECK(frame.free() == 1024);
 	}
@@ -87,8 +147,8 @@ TEST_CASE("arena", "[arena][memory]")
 	SECTION("arena allocate zero size")
 	{
 		memory::arena frame(1024);
-		void*         ptr1 = frame.allocate(0);
-		CHECK(ptr1 != nullptr);
+		auto          ptr1 = frame.allocate(0);
+		CHECK(ptr1.size() == 0);
 		CHECK(frame.used() == 0);
 		CHECK(frame.free() == 1024);
 	}
@@ -97,7 +157,7 @@ TEST_CASE("arena", "[arena][memory]")
 	SECTION("allocate block")
 	{
 		memory::arena frame(1024);
-		auto          ptr = frame.allocate_block<int>(8);
+		auto          ptr = frame.allocate<int>(8);
 
 		ptr[0] = 0xDEAD'BEEF;
 
@@ -201,7 +261,7 @@ TEST_CASE("arena", "[arena][memory]")
 			CHECK(p.z == 12.0f);
 		}
 
-		auto particles3 = frame.allocate_block<Particle>(2);
+		auto particles3 = frame.allocate<Particle>(2);
 		CHECK(particles3.size() == 2);
 
 		// #1
