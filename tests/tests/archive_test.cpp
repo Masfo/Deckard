e88dabef9d127c242a5dc0293c343891f2ad19d9
Toni Mäkinen <masfonos@gmail.com>
2026-08-04 05:15:59 +0300

    sqlite: remove in favor of own archive

---
+++ tests/tests/archive_test.cpp
@@ -0,0 +1,15 @@
+ #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+// #include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+
+import std;
+
+TEST_CASE("archive", "[archive]")
+{
+	SECTION("d")
+	{
+		//
+		CHECK(1 == 1);
+	}
+}
